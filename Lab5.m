063a73bff500264b97968b5f4a99d0caf291c9e2
Unknown <hbz15001@student.mdh.se>
2019-02-22 14:51:34 +0100

    Training with net
    Works quite badly

--- Lab5.m
+++ Lab5.m
@@ -1,7 +1,8 @@
 clc;
 clear;
+close all;
 toPlot = 1; %plot the first history of the cart and rod
-traindum = 0; %If 1 random starting position and trainduming. If 0 [0 0 0 0] and no trainduming
+traindum = 100; %If 1 random starting position and trainduming. If 0 [0 0 0 0] and no trainduming
 startState = [0 0 0 0];
 learnRate = 0.8;
 toPause = 0;
@@ -10,13 +11,13 @@ if(traindum == 0)
     toPause = 0.002;
 end
 
-x3 = -pi/15:pi/60:pi/15; %theta. The angle of the pendelum.  
-x4 = -pi/4:pi/8:pi/4; %theta dot. The angle velocity of the pendelum
 x1 = -2.4:1.2:2.4; %x pos. The position of the cart
 x2 = -2:1:2; %x dist dot. The speed of the cart
+x3 = -pi/15:pi/60:pi/15; %theta. The angle of the pendelum.  
+x4 = -pi/4:pi/8:pi/4; %theta dot. The angle velocity of the pendelum
 
 actions = [-10, 10]; % Either force backward or forward
-maxEpisodes = 1; %Max episodes of attempts
+maxEpisodes = 3; %Max episodes of attempts
 
 
 
@@ -49,28 +50,48 @@ hold off
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 TrSet = {};
 Tripplet = [];
-for example = 1:200
-    currentState = startState;
-    TrSet{1, example} = [currentState];
-    TrSet{2, example} = [0];
-
-    while(abs(currentState(1)) <= 2 && abs(currentState(3))<=pi/15)
-        action = 20*round(rand)-10;
-        nextState = SimulatePendel(action, currentState(1), currentState(2), currentState(3), currentState(4));
-        TrSet{1, example} = [TrSet{1, example} ; nextState];
-        TrSet{2, example} = [TrSet{2, example}; action];
-        currentState = nextState;
-    end
-    cost = (abs(TrSet{1,example}(:,3)) > pi/20 | (abs(TrSet{1,example}(:,1)) > 2.4));
-    Tripplet = [Tripplet; [TrSet{1,example}(1:end-1,:) TrSet{2,example}(2:end,:) cost(2:end)]];
-end
-QNeur;
-for episode = 1:maxEpisodes
-    if traindum == 0
+if(traindum ~=0)
+    for example = 1:100
         currentState = startState;
-    elseif traindum == 1
-        currentState = [-1.2 + (2.4)*rand, 0 ,-pi/40 + (pi/20)*rand, 0];
+        TrSet{1, example} = [currentState];
+        TrSet{2, example} = [0];
+
+        while(abs(currentState(1)) <= 2 && abs(currentState(3))<=pi/15)
+            action = 20*round(rand)-10;
+            nextState = SimulatePendel(action, currentState(1), currentState(2), currentState(3), currentState(4));
+            TrSet{1, example} = [TrSet{1, example} ; nextState];
+            TrSet{2, example} = [TrSet{2, example}; action];
+            currentState = nextState;
+        end
+        cost = (abs(TrSet{1,example}(:,3)) > pi/20 | (abs(TrSet{1,example}(:,1)) > 2.4));
+        Tripplet = [Tripplet; [TrSet{1,example}(1:end-1,:) TrSet{2,example}(2:end,:) cost(2:end)]];
     end
+    QNeur;
+    for(i = 1: traindum)
+        for example = 1:5
+            currentState = startState;
+            TrSet{1, example} = [currentState];
+            TrSet{2, example} = [0];
+            Q1 = [];
+            Q2 = []; 
+            while(abs(currentState(1)) <= 2 && abs(currentState(3))<=pi/15)
+                action = 20*round(rand)-10;
+                nextState = SimulatePendel(action, currentState(1), currentState(2), currentState(3), currentState(4));
+                TrSet{1, example} = [TrSet{1, example} ; nextState];
+                TrSet{2, example} = [TrSet{2, example}; action];
+                currentState = nextState;
+                Q1 = [Q1; net([currentState 10]')];
+                Q2 = [Q2; net([currentState -10]')];
+            end
+
+            cost = (abs(TrSet{1,example}(2:end,3)) > pi/20 | (abs(TrSet{1,example}(2:end,1)) > 2.4)) + 0.9*min(Q1, Q2);
+            Tripplet = [Tripplet; [TrSet{1,example}(1:end-1,:) TrSet{2,example}(2:end,:) cost]];
+        end
+       [net,tr] = train(net,Tripplet(:,1:5)', Tripplet(:,6)'); 
+    end
+end
+for episode = 1:maxEpisodes
+    currentState = startState;
     index = 0;
 
     while(abs(currentState(1)) <= 2.4 && abs(currentState(3))<=pi/15)
@@ -82,7 +103,6 @@ for episode = 1:maxEpisodes
  
         %%%%%%%%%%%%%%%%%%%%%
         %Simulate
-        %%%%%%%%%%%%%%%%%%%%%
         Q1 = net([currentState 10]');
         Q2 = net([currentState -10]');
         if(Q1 > Q2)
@@ -95,7 +115,7 @@ for episode = 1:maxEpisodes
         %%%%%%%%%%%%%%%%%%%%%%
         %traindum on result
         %%%%%%%%%%%%%%%%%%%%%%
-       
+        
        
         currentState = nextState;
         clc;
