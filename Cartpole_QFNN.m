51f57be0930443caeaf54c4b587e39d4ac67ac50
Unknown <hbz15001@student.mdh.se>
2019-03-01 14:18:46 +0100

    qw789poiytre

--- Cartpole_QFNN.m
+++ Cartpole_QFNN.m
@@ -1,6 +1,6 @@
 %One script to rule them all
-
-
+clear;
+close all;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Cartpole state limiters and actions constants
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -45,72 +45,26 @@ TrSet = {};
 Input = [];
 goal = 100;
 nrOfActions = 0;
-while(1)
-    clc;
-    if goal <= nrOfActions
-        break;
-    end
-    
-    for i = 1:200
-        currentState = startState;
-        % State array
-        TrSet{1, i} = [currentState];
-        % Action array
-        % First index in the action array is never used but needed due to size issues when expanding the episode
-        TrSet{2, i} = [0];
+net =fitnet([5 5],'trainrp');
 
-        while(abs(currentState(1)) <= deathCartPos && abs(currentState(3))<=deathPoleAngle)
-            action = actions(floor(4*rand)+1);
-            % Add following for random noise: *(1 + 0.2*(2*rand - 1))
-            nextState = SimulatePendel(action*(1 + 0.1*(2*rand - 1)), currentState(1), currentState(2), currentState(3), currentState(4));
-            TrSet{1, i} = [TrSet{1, i} ; nextState];
-            TrSet{2, i} = [TrSet{2, i}; action];
-            currentState = nextState;
-        end
-        % Calculate the costs of the future state, use 0.1/0.9 instead of 0/1 to accomodate sigmoid function.
-        Target = 0.05 + 0.90*(abs(TrSet{1,i}(:,3)) > allowedPoleAngle | abs(TrSet{1,i}(:,1)) > allowedCartPos);
-        % Recursivly expand the 'input' cell array with 'state, action, cost' - tripplets
-        Input = [Input; TrSet{1,i}(1:end-1,:) TrSet{2,i}(2:end,:) Target(2:end)];
-    end
+net.trainParam.showWindow = false;
+% Setup Division of Data for Training, Validation, Testing | help nndivision
+net.divideFcn = 'dividerand';  % Divide data randomly
+net.divideMode = 'sample';  % Divide up every sample
+net.divideParam.trainRatio = 90/100;
+net.divideParam.valRatio = 5/100;
+net.divideParam.testRatio = 5/100;
 
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    % Initiate network
-    % Random weight/bias initiation
-    % Training fuction can be set inside initNN()
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    net = initNN(Input(:,1:5)', Input(:,6)', [5 5]);
-    
-    currentState = startState;
-    i = 0;
-    while(abs(currentState(1)) <= deathCartPos && abs(currentState(3))<=deathPoleAngle)
-        i = i + 1;
-
-        %set(f,'XData',[currentState(1) currentState(1)+ sin(currentState(3))]); %x pos of stick
-        %set(g,'XData',currentState(1)); %x pos of dot
-        %set(f,'YData',[0 cos(currentState(3))]); %y pos of stick
-
-        if(mod(i, 3) == 0)
-            action = 10*rand - 5;
-        else
-            Q1 = net([currentState actions(1)]');
-            Q2 = net([currentState actions(2)]');
-            Q3 = net([currentState actions(3)]');
-            Q4 = net([currentState actions(4)]');
-
-            [mv, index] = min([Q1 Q2 Q3 Q4]);
-            action = actions(index);
-        end
-        nextState = SimulatePendel(action*(1 + 0.2*(2*rand - 1)), currentState(1), currentState(2), currentState(3), currentState(4));
-        currentState = nextState;
-        nrOfActions = i;
-    end
-end
+% Choose a Performance Function | help nnperformance
+%net.performFcn = 'mse';  % Mean Squared Error
+net.performFcn = 'mse';
+[net,tr] = train(net,zeros(10,5)',zeros(10,1)');
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Q-fitted training
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% Used to indicate savepoints of network 
+% Used to indicate savepoints of network
 bestActionNr = 0;
 % Goal of the training
 maxRange = 2000;
@@ -126,19 +80,17 @@ for k = 1:15
             
             % Exploration 1/(epsilon) of the time, set false if no exploration
             epsilon = 5+k;
-            if(mod(actionNr, epsilon) == 0 && true)
-                action = actions(floor(4*rand)+1);
-            else
-                Q1 = net([currentState actions(1)]');
-                Q2 = net([currentState actions(2)]');
-                Q3 = net([currentState actions(3)]');
-                Q4 = net([currentState actions(4)]');
-                
-                [mv, index] = min([Q1 Q2 Q3 Q4]);
-                action = actions(index);
-            end
+            action = actions(floor(4*rand)+1);
+            %                 Q1 = net([currentState actions(1)]');
+            %                 Q2 = net([currentState actions(2)]');
+            %                 Q3 = net([currentState actions(3)]');
+            %                 Q4 = net([currentState actions(4)]');
+            
+            %    [mv, index] = min([net([currentState actions(1)]') net([currentState actions(2)]') net([currentState actions(3)]') net([currentState actions(4)]')]);
+            %    action = actions(index);
+            
             %Next state is suspect to a random noise with magnitude 20% of action '*(1 + 0.2*(2*rand - 1))'
-            nextState = SimulatePendel(action*(1 + 0.2*(2*rand - 1)), currentState(1), currentState(2), currentState(3), currentState(4));
+            nextState = SimulatePendel(action, currentState(1), currentState(2), currentState(3), currentState(4));
             TrSet{1, i} = [TrSet{1, i} ; nextState];
             TrSet{2, i} = [TrSet{2, i}; action];
             currentState = nextState;
@@ -157,7 +109,7 @@ for k = 1:15
             %Set display of external loop on
             if (true)
                 disp('Training Iteration: ')
-                disp(k-1) 
+                disp(k-1)
             end
         end
         
@@ -168,13 +120,14 @@ for k = 1:15
         end
         % Calculate cost of next state
         %Target = min(net([TrSet{1,i}(:,1:4)' actions(1)]), net([TrSet{1,i}(:,1:4)' actions(2)]), net([TrSet{1,i}(:,1:4)' actions(3)]), net([TrSet{1,i}(:,1:4)' actions(4)]));
-         %for(minil = 1:lenght(TrSet{1,i}(1,:)))
-         minimididadta = min(min(net([TrSet{1,i}(2:end,:) actions(1)*ones(length(TrSet{1,i}(2:end,1)),1)]'),net([TrSet{1,i}(2:end,:) actions(2)*ones(length(TrSet{1,i}(2:end,1)),1)]')),min(net([TrSet{1,i}(2:end,:) actions(3)*ones(length(TrSet{1,i}(2:end,1)),1)]'),net([TrSet{1,i}(2:end,:) actions(4)*ones(length(TrSet{1,i}(2:end,1)),1)]')))';
-%             Q3 = net([currentState actions(3)]');
-%             Q4 = net([currentState actions(4)]');
-                %net([TrSet{1,i}(2:end,:) actions(2)*ones(length(TrSet{1,i}(2:end,1)),1)]')
+        %for(minil = 1:lenght(TrSet{1,i}(1,:)))
+        minimididadta = min([net([TrSet{1,i}(2:end,:) actions(1)*ones(length(TrSet{1,i}(2:end,1)),1)]') net([TrSet{1,i}(2:end,:) actions(2)*ones(length(TrSet{1,i}(2:end,1)),1)]') net([TrSet{1,i}(2:end,:) actions(3)*ones(length(TrSet{1,i}(2:end,1)),1)]') net([TrSet{1,i}(2:end,:) actions(4)*ones(length(TrSet{1,i}(2:end,1)),1)]')])';
+        %             Q3 = net([currentState actions(3)]');
+        %             Q4 = net([currentState actions(4)]');
+        %net([TrSet{1,i}(2:end,:) actions(2)*ones(length(TrSet{1,i}(2:end,1)),1)]')
         Target = 0.05 + 0.1*(abs(TrSet{1,i}(2:end,3)) > allowedPoleAngle | abs(TrSet{1,i}(2:end,1)) > allowedCartPos) + 0.9*minimididadta;
-        Input = [Input; [TrSet{1,i}(1:end-1,:) TrSet{2,i}(2:end,:)] Target];
+        Input = [Input; [TrSet{1,i}(1:end-1,:) TrSet{2,i}(2:end,:)]];
+        adapt(net, Input',Target');
         if actionNr == 1000
             speedUp = speedUp + 1;
         end
@@ -182,7 +135,7 @@ for k = 1:15
             break;
         end
     end
-    [net,tr] = train(net,Input(:,1:5)', Input(:,6)');
+    %[net,tr] = train(net,Input(:,1:5)', Input(:,6)');
 end
 net = bestNet;
 
@@ -210,7 +163,7 @@ while(abs(currentState(1)) <= deathCartPos && abs(currentState(3))<=deathPoleAng
         Q2 = net([currentState actions(2)]');
         Q3 = net([currentState actions(3)]');
         Q4 = net([currentState actions(4)]');
-
+        
         [mv, index] = min([Q1 Q2 Q3 Q4]);
         action = actions(index);
     end
